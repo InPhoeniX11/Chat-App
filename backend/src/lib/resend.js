a36c8013bd0fb449c4ccabc1a9bcaa7744259c0d
InPhoeniX11 <sudhanshusingh04251@gmail.com>
2026-01-20 01:20:45 +0530

    updated

--- backend/src/lib/resend.js
+++ backend/src/lib/resend.js
@@ -1,9 +1,30 @@
+// import { Resend } from "resend";
+// import { ENV } from "./env.js";
+
+// export const resendClient = new Resend(ENV.RESEND_API_KEY);
+
+// export const sender = {
+//   email: ENV.EMAIL_FROM,
+//   name: ENV.EMAIL_FROM_NAME,
+// };
+
+
 import { Resend } from "resend";
 import { ENV } from "./env.js";
 
-export const resendClient = new Resend(ENV.RESEND_API_KEY);
+let resendClient = null;
+
+if (ENV.NODE_ENV === "production") {
+  if (!ENV.RESEND_API_KEY) {
+    throw new Error("RESEND_API_KEY is missing in production");
+  }
+
+  resendClient = new Resend(ENV.RESEND_API_KEY);
+}
+
+export { resendClient };
 
 export const sender = {
-  email: ENV.EMAIL_FROM,
-  name: ENV.EMAIL_FROM_NAME,
+  email: ENV.EMAIL_FROM || "no-reply@example.com",
+  name: ENV.EMAIL_FROM_NAME || "App",
 };
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development" // use the url localhost... if development mode, else send the dynamic route
-      ? "http://localhost:3000/api"
+      ? "http://localhost:5000/api"
       : "/api",
   withCredentials: true, //send cookies with the request
 });
